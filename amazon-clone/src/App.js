436990accb9cd794e22608bbd644922a09cbd553
KarmiTenchi <73952811+KarmiTenchi@users.noreply.github.com>
2022-09-21 05:08:31 +0300

    header commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        {/* Home */}
+      </div>
+    );
+  }
+}
+
+export default App;
