bf64c87fa83a4a0b4a55490b1f08e870043fcf01
KarmiTenchi <73952811+KarmiTenchi@users.noreply.github.com>
2022-09-21 05:06:49 +0300

    Initial commit

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKm_-m-77TrSfyaV9k3v3CjV4x027rKpE",
+  authDomain: "challenge-fdacb.firebaseapp.com",
+  projectId: "challenge-fdacb",
+  storageBucket: "challenge-fdacb.appspot.com",
+  messagingSenderId: "220707500407",
+  appId: "1:220707500407:web:eaba52a88247102ddc3172",
+  measurementId: "G-XT8L6T3JFH",
+};
